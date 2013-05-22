1d57b4c23ab0df019dbabdc436e7b19110bf54c8
Maurogik <gwenn.aubert@gmail.com>
2013-05-22 15:24:16 +0200

    das

--- ROAR/src/core/DataServer.java
+++ ROAR/src/core/DataServer.java
@@ -435,8 +435,10 @@ public class DataServer implements Serializable{
 		Collections.sort(res);
 		
 		try{
-			if(cl.getNbMessageRead() >= nbMessages){
+			if(cl.getNbMessageRead() >= nbMessages && res.size() > nbMessages){
+				
 				res = res.subList(cl.getNbMessageRead(), cl.getNbMessageRead()+5);
+
 			} else {
 				cl.pullFinished();
 			}
